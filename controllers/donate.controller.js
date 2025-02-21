48bbe43dd8b09d2cc4ffeacb945141466a5887e7
Mokshita Kochhar <mokshita.kochhar07@gmail.com>
2025-02-21 13:00:35 +0530

    up to date

--- controllers/donate.controller.js
+++ controllers/donate.controller.js
@@ -1,11 +1,11 @@
 import { Donors} from "../models/Donor.models.js"
-import { NGO } from "../models/Ngo.models.js";
+import {NGO} from "../models/Ngo.models.js";
 import { Requests } from "../models/Requests.models.js";
 import { Notification } from "../models/Notifications.models.js";
 
-  
+
 //donate request is generated
-const sendDonationRequest = async(req, res) => {
+const sendDonationRequest = async(req, res, io) => {
     // MongoDB automatically provides a unique _id for every document.
     
     const donorId = req.body.donorId;
@@ -51,7 +51,7 @@ const sendDonationRequest = async(req, res) => {
     
 
     // Provide the generated notifications to mokshita 
-    // io.emit("newDonationRequest", notifications);
+    io.emit("newDonationRequest", notifications);
 
 
     // store this particular request in requestschema to show available requests to ngo
@@ -74,7 +74,6 @@ const sendDonationRequest = async(req, res) => {
 
 export {sendDonationRequest};
 
-export {sendDonationRequest};
 
 
 
