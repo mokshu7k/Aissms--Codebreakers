23e30a1c979f7d98920c11149ddf97f4af498873
Chatlalg <chatlalg@gmail.com>
2025-02-23 03:38:38 +0530

    write cron job

--- jobs/expirycheck.js
+++ jobs/expirycheck.js
@@ -1,18 +1,18 @@
 import Requests  from "../models/Requests.models.js";
 import cron from "node-cron";
 // Runs every hour 
-// cron.schedule("0 * * * *", async () => {
-//     console.log("Running expiry check...");
+cron.schedule("0 * * * *", async () => {
+    console.log("Running expiry check...");
 
-//     const now = new Date();
-//     const result = await Requests.updateMany(
-//         { 
-//             expirydate: { $lt: now }, status: "pending" 
-//         },
-//         { 
-//             status: "expired" 
-//         }
-//     );
+    const now = new Date();
+    const result = await Requests.updateMany(
+        { 
+            expirydate: { $lt: now }, status: "pending" 
+        },
+        { 
+            status: "expired" 
+        }
+    );
 
     console.log(`Marked ${result.modifiedCount} requests as expired.`);
 });
